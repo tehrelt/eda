4e6a0a11cac00aa712b79266740a72f24dedb237
Dmitry Evteev <haxrelt@gmail.com>
2024-10-11 00:19:33 +0300

    fix: move customers to foreign table

--- orders/migrations/000001_init.down.sql
+++ orders/migrations/000001_init.down.sql
@@ -1,3 +1,5 @@
 drop table if exists orders;
 
+drop table if exists customers;
+
 drop extension if exists "uuid-ossp";
--- orders/migrations/000001_init.up.sql
+++ orders/migrations/000001_init.up.sql
@@ -1,8 +1,15 @@
 create extension if not exists "uuid-ossp";
 
+create table if not exists customers (
+  id uuid primary key default uuid_generate_v4(),
+  username varchar not null,
+  created_at timestamp default now(),
+  updated_at timestamp
+);
+
 create table if not exists orders (
   id uuid primary key default uuid_generate_v4(),
-  who varchar not null,
+  customer_id uuid not null references customers(id),
   total decimal(10, 2) not null,
   created_at timestamp default now(),
   updated_at timestamp
